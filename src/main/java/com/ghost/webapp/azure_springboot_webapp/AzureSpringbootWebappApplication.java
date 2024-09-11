c582a578593d6acb871657fde0321bc026de07d9
Akshay Pansare <akshay.pansare@optum.com>
2024-09-11 16:41:10 +0530

    first commit

---
+++ src/main/java/com/ghost/webapp/azure_springboot_webapp/AzureSpringbootWebappApplication.java
@@ -0,0 +1,15 @@
+package com.ghost.webapp.azure_springboot_webapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@SpringBootApplication
+public class AzureSpringbootWebappApplication {
+
+
+	public static void main(String[] args) {
+		SpringApplication.run(AzureSpringbootWebappApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/ghost/webapp/azure_springboot_webapp/controller/AzureWebAppRestController.java
@@ -0,0 +1,15 @@
+package com.ghost.webapp.azure_springboot_webapp.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class AzureWebAppRestController {
+
+    @GetMapping("/test")
+    public String test(){
+        return "Welcome to Spring boot application deployment on azure web app";
+    }
+
+
+}
